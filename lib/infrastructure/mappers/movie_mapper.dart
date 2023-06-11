097eedf2975e31e5e65d08d63d4fd7e4823fcc9d
garguidevsw <garguidevsw@gmail.com>
2023-06-11 13:35:56 -0600

    Add Consumer Data from TheMovieDB + RiverPod

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBEntity(MovieMovieDB moviedb) => Movie(
+        adult: moviedb.adult,
+        backdropPath: (moviedb.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+            : 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/9556d16312333.5691dd2255721.jpg',
+        genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: (moviedb.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+            : 'no-poster',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+}
